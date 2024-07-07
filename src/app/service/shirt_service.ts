2f92ca75d034719de7c6d9208eca1b2de9c9540c
MinhTranTuan3112 <minhttse172842@fpt.edu.vn>
2024-07-07 11:56:05 +0700

    Update fetch shirt details

--- src/app/service/shirt_service.ts
+++ src/app/service/shirt_service.ts
@@ -2,6 +2,14 @@ export const fetchPagedShirts = async (page: number = 1, pageSize: number = 10) 
     const response = await fetch(`http://localhost:8080/api/shirts?pageNumber=${page}&pageSize=${pageSize}`);
     const data = await response.json();
     console.log(`Shirts data:`);
-    console.log({data});
+    console.log({ data });
+    return data;
+};
+
+export const fetchShirtDetails = async (id: number) => {
+    const response = await fetch(`http://localhost:8080/api/shirts/${id}`);
+    const data = await response.json();
+    console.log(`Shirt details:`);
+    console.log({ data });
     return data;
 };
